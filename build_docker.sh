a2e34d590401b487a35333d04dbc2633af1762c8
grigorimaxim <grigorimaxim422@gmail.com>
2025-03-13 18:40:45 +0000

    pre

--- build_docker.sh
+++ build_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker system prune -a --volumes
 #!/bin/bash
-
+rm -rf btcpredict2/
 VERSION=$1  # Pass version as an argument
 REPO="grigorimaxim/btcpredict"  # Change this to your repository
 
@@ -15,8 +15,8 @@ docker build -t $REPO:$VERSION .
 docker push $REPO:$VERSION
 
 
-docker save $REPO:$VERSION | gzip > btcpredict.tar.gz
-echo "Saved into btcpredict.tar.gz"
+docker save $REPO:$VERSION | gzip > btcpredict2.tar.gz
+echo "Saved into btcpredict2.tar.gz"
 
 # Optionally tag as latest
 docker tag $REPO:$VERSION $REPO:latest
@@ -25,12 +25,24 @@ docker push $REPO:latest
 echo "Image $REPO:$VERSION pushed successfully!"
 
 echo "Prepare uploading big file to hub..."
-#huggingface-cli repo create btcpredict
+# huggingface-cli repo create btcpredict
 
+# git lfs install
+huggingface-cli upload grigorimaxim/btcpredict ./btcpredict2.tar.gz
+
+huggingface-cli repo create btcpredict2 #if not created yet
+
+git clone https://huggingface.co/grigorimaxim/btcpredict2
+git config user.name grigorimaxim
+git config user.email grigorimaxim422@gmail.com
+cd btcpredict2
 git lfs install
-huggingface-cli upload grigorimaxim/merlin_tts ./btcpredict.tar.gz
-
-
+git lfs track "*.bin"  # Track specific file types
+git lfs track "*.gz"  # Track specific file types
+git add .gitattributes  # Ensure LFS settings are committed
+git add btcpredict2.tar.gz
+git commit -m "upload bigger"
+git push
 # git clone https://huggingface.co/grigorimaxim/parler58
 # cd parler58
 # sudo apt install git-lfs
